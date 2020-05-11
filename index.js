ced8446e88479cd1c49d080c8a12af6f55b6e25b
elfexor <imbalance777@gmail.com>
2020-05-11 21:56:45 +0300

    refactor

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const surnames_female = require('./json/surnames_female.json');
 const random = (max = 100) => Math.floor(Math.random() * (Math.floor(max) - 1)) + 1;
 
 const transliteration = (str) =>  {
-  const arr = { 'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ж':'g', 'з':'z', 'и':'i', 'й':'y', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'ы':'i', 'э':'e', 'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ж':'G', 'З':'Z', 'И':'I', 'Й':'Y', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O', 'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Ы':'I', 'Э':'E', 'ё':'yo', 'х':'h', 'ц':'ts', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ь':'', 'ю':'yu', 'я':'ya', 'Ё':'Yo', 'Х':'H', 'Ц':'TS', 'Ч':'Ch', 'Ш':'Sh', 'Щ':'Shch', 'Ъ':'', 'Ь':'', 'Ю':'Yu', 'Я':'Ya' };
-  const replacer = a => arr[a] || '';
-  return str.replace(/[А-яёЁ]/g, replacer)
+  const characters = { 'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ж':'g', 'з':'z', 'и':'i', 'й':'y', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'ы':'i', 'э':'e', 'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ж':'G', 'З':'Z', 'И':'I', 'Й':'Y', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O', 'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Ы':'I', 'Э':'E', 'ё':'yo', 'х':'h', 'ц':'ts', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ь':'', 'ю':'yu', 'я':'ya', 'Ё':'Yo', 'Х':'H', 'Ц':'TS', 'Ч':'Ch', 'Ш':'Sh', 'Щ':'Shch', 'Ъ':'', 'Ь':'', 'Ю':'Yu', 'Я':'Ya' };
+  return str.replace(/[А-яёЁ]/g, character => characters[character] || '');
 }
 
-const generate = (gender) =>  {
+const create = (gender) =>  {
   gender = ['male', 'female'].includes(gender) ? gender : ['male', 'female'][Math.round(Math.random())];
 
   const name = gender === 'male' ? names_male[random(names_male.length)].name : names_female[random(names_female.length)].name;
@@ -32,11 +31,11 @@ const generate = (gender) =>  {
   }
 }
 
-generate.many = (amount = 10, gender) => {
+const many = (amount = 10, gender) => {
   let counter = 0, names = [];
 
   do {
-    names = [...names, generate(gender)];
+    names = [...names, create(gender)];
     counter = counter + 1;
   } while (amount > counter);
 
@@ -44,6 +43,6 @@ generate.many = (amount = 10, gender) => {
 }
 
 module.exports = {
-  one: (gender = null) => generate(gender),
-  many: (amount, gender) => generate.many(amount, gender),
+  one: (gender = null) => create(gender),
+  many: (amount, gender) => many(amount, gender),
 }
