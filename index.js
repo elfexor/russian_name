68a30de13a38f71357cd7520626cb2bdadc9256f
Вадим Васильевич <imbalance777@gmail.com>
2020-09-25 10:36:05 +0300

    refactor

--- index.js
+++ index.js
@@ -5,16 +5,90 @@ const surnames_female = require('./json/surnames_female.json');
 
 const random = (max = 100) => Math.floor(Math.random() * (Math.floor(max) - 1)) + 1;
 
-const transliteration = (str) =>  {
-  const characters = { 'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ж':'g', 'з':'z', 'и':'i', 'й':'y', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'ы':'i', 'э':'e', 'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ж':'G', 'З':'Z', 'И':'I', 'Й':'Y', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O', 'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Ы':'I', 'Э':'E', 'ё':'yo', 'х':'h', 'ц':'ts', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ь':'', 'ю':'yu', 'я':'ya', 'Ё':'Yo', 'Х':'H', 'Ц':'TS', 'Ч':'Ch', 'Ш':'Sh', 'Щ':'Shch', 'Ъ':'', 'Ь':'', 'Ю':'Yu', 'Я':'Ya' };
-  return str.replace(/[А-яёЁ]/g, character => characters[character] || '');
-}
+const transliteration = (str = '') => {
+  const characters = new Map([
+    ['а', 'a'],
+    ['б', 'b'],
+    ['в', 'v'],
+    ['г', 'g'],
+    ['д', 'd'],
+    ['е', 'e'],
+    ['ж', 'g'],
+    ['з', 'z'],
+    ['и', 'i'],
+    ['й', 'y'],
+    ['к', 'k'],
+    ['л', 'l'],
+    ['м', 'm'],
+    ['н', 'n'],
+    ['о', 'o'],
+    ['п', 'p'],
+    ['р', 'r'],
+    ['с', 's'],
+    ['т', 't'],
+    ['у', 'u'],
+    ['ф', 'f'],
+    ['ы', 'i'],
+    ['э', 'e'],
+    ['А', 'A'],
+    ['Б', 'B'],
+    ['В', 'V'],
+    ['Г', 'G'],
+    ['Д', 'D'],
+    ['Е', 'E'],
+    ['Ж', 'G'],
+    ['З', 'Z'],
+    ['И', 'I'],
+    ['Й', 'Y'],
+    ['К', 'K'],
+    ['Л', 'L'],
+    ['М', 'M'],
+    ['Н', 'N'],
+    ['О', 'O'],
+    ['П', 'P'],
+    ['Р', 'R'],
+    ['С', 'S'],
+    ['Т', 'T'],
+    ['У', 'U'],
+    ['Ф', 'F'],
+    ['Ы', 'I'],
+    ['Э', 'E'],
+    ['ё', 'yo'],
+    ['х', 'h'],
+    ['ц', 'ts'],
+    ['ч', 'ch'],
+    ['ш', 'sh'],
+    ['щ', 'shch'],
+    ['ъ', ''],
+    ['ь', ''],
+    ['ю', 'yu'],
+    ['я', 'ya'],
+    ['Ё', 'Yo'],
+    ['Х', 'H'],
+    ['Ц', 'TS'],
+    ['Ч', 'Ch'],
+    ['Ш', 'Sh'],
+    ['Щ', 'Shch'],
+    ['Ъ', ''],
+    ['Ь', ''],
+    ['Ю', 'Yu'],
+    ['Я', 'Ya'],
+  ]);
 
-const create = (gender) =>  {
+  return str.replace(/[А-яёЁ]/g, (character) => characters.get(character) || '');
+};
+
+const create = (gender) => {
   gender = ['male', 'female'].includes(gender) ? gender : ['male', 'female'][Math.round(Math.random())];
 
-  const name = gender === 'male' ? names_male[random(names_male.length - 1)].name : names_female[random(names_female.length - 1)].name;
-  const surname = gender === 'male' ? surnames_male[random(surnames_male.length - 1)].surname : surnames_female[random(surnames_female.length - 1)].surname;
+  const name =
+    gender === 'male'
+      ? names_male[random(names_male.length - 1)].name
+      : names_female[random(names_female.length - 1)].name;
+  const surname =
+    gender === 'male'
+      ? surnames_male[random(surnames_male.length - 1)].surname
+      : surnames_female[random(surnames_female.length - 1)].surname;
 
   const fullname = `${name} ${surname}`;
 
@@ -26,13 +100,14 @@ const create = (gender) =>  {
     transliteration: {
       name: transliteration(name),
       surname: transliteration(surname),
-      fullname:transliteration(fullname)
-    }
-  }
-}
+      fullname: transliteration(fullname),
+    },
+  };
+};
 
 const many = (amount = 10, gender) => {
-  let counter = 0, names = [];
+  let counter = 0,
+    names = [];
 
   do {
     names = [...names, create(gender)];
@@ -40,9 +115,9 @@ const many = (amount = 10, gender) => {
   } while (amount > counter);
 
   return names;
-}
+};
 
 module.exports = {
   one: (gender = null) => create(gender),
   many: (amount, gender) => many(amount, gender),
-}
+};
